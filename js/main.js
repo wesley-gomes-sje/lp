b45e38517b5acda2899d1503a8c28d7d68d5b027
wesley-gomes-sje <57814784+wesley-gomes-sje@users.noreply.github.com>
2024-01-08 19:58:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/main.js
@@ -0,0 +1,81 @@
+/* Abrir o menu */
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/* remover menu mobile*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*mudando o fundo do header*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');  else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll',scrollHeader)
+
+
+
+
+
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >=200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+
+
+
+
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+const sr = ScrollReveal({
+    distance:'60px',
+    duration: 2500,
+    delay: 400,
+})
+sr.reveal(`.home__header, .section__title`,{delay:600})
+sr.reveal(`.home__footer`, {delay:700})
+sr.reveal(`.home__img`, {delay:900,origin:'top'})
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy, .imgphone`, {origin:'top', interval:100})
+sr.reveal(`.specs__data`, {origin:'left', interval:100})
+sr.reveal(`.specs__img, .phone__data`, {origin:'right'})
